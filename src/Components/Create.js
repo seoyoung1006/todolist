8f4cc5c830c00691206cabc8886e86d0a94f03c7
toystory <74535088+seoyoung1006@users.noreply.github.com>
2022-08-15 16:41:57 +0900

    Update

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -12,18 +12,19 @@ class Create extends Component{
     render(){
         return(
             <div>
-                <p><input type="submit" value="+ Catagory" id="btn2"
+                <p className="toggle"><input type="submit" value="+ Catagory" id="btn2"
                 onClick={function(){this.toggleBtn()}.bind(this)}
                 ></input></p>
                 <div id="btn1">
                     <form action="/create_process" method="post" onSubmit={function(e){
                         e.preventDefault();
+                        console.log(e);
                         this.props.onSubmit(
                             e.target.title.value,
                             e.target.desc.value
-                        )
+                        );
                     }.bind(this)}>
-                        <p><input type="text" name="catagory" placeholder="할일을 입력해주세요"></input></p>
+                        <p><input type="text" name="title" placeholder="할일을 입력해주세요"></input></p>
                         <p><textarea name="desc" placeholder="할일의 구체적인 계획"></textarea></p>
                         <input type="submit" value="ADD"></input>
                     </form>
